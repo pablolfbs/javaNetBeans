a358b8adea0477c34c16fbd64c9a431f3f68c435
pablolfbs <pablo.fariabritto@gmail.com>
2018-06-12 21:15:50 -0300

    updates

--- InterfaceDispositivosMoveis/build/web/script.js
+++ InterfaceDispositivosMoveis/build/web/script.js
@@ -4,14 +4,74 @@
  * and open the template in the editor.
  */
 
-function processar1() {
-    document.getElementById("option2").selected="true";
-    document.getElementById("selectDoPeriodo2").disabled="true";
-    document.getElementById("selectDoPeriodo1").disabled="false";
+function processar1(){
+    document.getElementById("selectDoPeriodo1").disabled = false;
+    document.getElementById("option2").selected = true;
+    document.getElementById("option3").selected = true;
+    document.getElementById("option4").selected = true;
+    document.getElementById("option5").selected = true;
+    document.getElementById("selectDoPeriodo2").disabled = true;
+    document.getElementById("selectDoPeriodo3").disabled = true;
+    document.getElementById("selectDoPeriodo4").disabled = true;
+    document.getElementById("selectDoPeriodo5").disabled = true;
 }
 
-function processar2() {
-    document.getElementById("selectDoPeriodo2").disabled="false";
-    document.getElementById("option1").selected="true";
-    document.getElementById("selectDoPeriodo1").disabled="true";
+function processar2(){
+    document.getElementById("selectDoPeriodo2").disabled = false;
+    document.getElementById("option1").selected = true;
+    document.getElementById("option3").selected = true;
+    document.getElementById("option4").selected = true;
+    document.getElementById("option5").selected = true;
+    document.getElementById("selectDoPeriodo1").disabled = true;
+    document.getElementById("selectDoPeriodo3").disabled = true;
+    document.getElementById("selectDoPeriodo4").disabled = true;
+    document.getElementById("selectDoPeriodo5").disabled = true;
 }
+
+function processar3(){
+    document.getElementById("selectDoPeriodo3").disabled = false;
+    document.getElementById("option1").selected = true;
+    document.getElementById("option2").selected = true;
+    document.getElementById("option4").selected = true;
+    document.getElementById("option5").selected = true;
+    document.getElementById("selectDoPeriodo1").disabled = true;
+    document.getElementById("selectDoPeriodo2").disabled = true;
+    document.getElementById("selectDoPeriodo4").disabled = true;
+    document.getElementById("selectDoPeriodo5").disabled = true;
+}
+
+function processar4(){
+    document.getElementById("selectDoPeriodo4").disabled = false;
+    document.getElementById("option1").selected = true;
+    document.getElementById("option2").selected = true;
+    document.getElementById("option3").selected = true;
+    document.getElementById("option5").selected = true;
+    document.getElementById("selectDoPeriodo1").disabled = true;
+    document.getElementById("selectDoPeriodo2").disabled = true;
+    document.getElementById("selectDoPeriodo3").disabled = true;
+    document.getElementById("selectDoPeriodo5").disabled = true;
+}
+
+function processar5(){
+    document.getElementById("selectDoPeriodo5").disabled = false;
+    document.getElementById("option1").selected = true;
+    document.getElementById("option2").selected = true;
+    document.getElementById("option3").selected = true;
+    document.getElementById("option4").selected = true;
+    document.getElementById("selectDoPeriodo1").disabled = true;
+    document.getElementById("selectDoPeriodo2").disabled = true;
+    document.getElementById("selectDoPeriodo3").disabled = true;
+    document.getElementById("selectDoPeriodo4").disabled = true;
+}
+
+//function processar1() {
+//    document.getElementById("option2").selected="true";
+//    document.getElementById("selectDoPeriodo2").disabled="true";
+//    document.getElementById("selectDoPeriodo1").disabled="false";
+//}
+//
+//function processar2() {
+//    document.getElementById("selectDoPeriodo2").disabled="false";
+//    document.getElementById("option1").selected="true";
+//    document.getElementById("selectDoPeriodo1").disabled="true";
+//}
