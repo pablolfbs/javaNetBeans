1562ecc0cf48cca3c8d559d585fb0213f29118a8
pablolfbs <pablo.fariabritto@gmail.com>
2018-06-12 22:01:51 -0300

    updates

--- InterfaceDispositivosMoveis/web/script.js
+++ InterfaceDispositivosMoveis/web/script.js
@@ -4,65 +4,102 @@
  * and open the template in the editor.
  */
 
-function processar1(){
-    document.getElementById("selectDoPeriodo1").disabled = false;
-    document.getElementById("option2").selected = true;
-    document.getElementById("option3").selected = true;
-    document.getElementById("option4").selected = true;
-    document.getElementById("option5").selected = true;
-    document.getElementById("selectDoPeriodo2").disabled = true;
-    document.getElementById("selectDoPeriodo3").disabled = true;
-    document.getElementById("selectDoPeriodo4").disabled = true;
-    document.getElementById("selectDoPeriodo5").disabled = true;
+//function processar1(){
+//    document.getElementById("selectDoPeriodo1").disabled = false;
+//    document.getElementById("option2").selected = true;
+//    document.getElementById("option3").selected = true;
+//    document.getElementById("option4").selected = true;
+//    document.getElementById("option5").selected = true;
+//    document.getElementById("selectDoPeriodo2").disabled = true;
+//    document.getElementById("selectDoPeriodo3").disabled = true;
+//    document.getElementById("selectDoPeriodo4").disabled = true;
+//    document.getElementById("selectDoPeriodo5").disabled = true;
+//}
+//
+//function processar2(){
+//    document.getElementById("selectDoPeriodo2").disabled = false;
+//    document.getElementById("option1").selected = true;
+//    document.getElementById("option3").selected = true;
+//    document.getElementById("option4").selected = true;
+//    document.getElementById("option5").selected = true;
+//    document.getElementById("selectDoPeriodo1").disabled = true;
+//    document.getElementById("selectDoPeriodo3").disabled = true;
+//    document.getElementById("selectDoPeriodo4").disabled = true;
+//    document.getElementById("selectDoPeriodo5").disabled = true;
+//}
+//
+//function processar3(){
+//    document.getElementById("selectDoPeriodo3").disabled = false;
+//    document.getElementById("option1").selected = true;
+//    document.getElementById("option2").selected = true;
+//    document.getElementById("option4").selected = true;
+//    document.getElementById("option5").selected = true;
+//    document.getElementById("selectDoPeriodo1").disabled = true;
+//    document.getElementById("selectDoPeriodo2").disabled = true;
+//    document.getElementById("selectDoPeriodo4").disabled = true;
+//    document.getElementById("selectDoPeriodo5").disabled = true;
+//}
+//
+//function processar4(){
+//    document.getElementById("selectDoPeriodo4").disabled = false;
+//    document.getElementById("option1").selected = true;
+//    document.getElementById("option2").selected = true;
+//    document.getElementById("option3").selected = true;
+//    document.getElementById("option5").selected = true;
+//    document.getElementById("selectDoPeriodo1").disabled = true;
+//    document.getElementById("selectDoPeriodo2").disabled = true;
+//    document.getElementById("selectDoPeriodo3").disabled = true;
+//    document.getElementById("selectDoPeriodo5").disabled = true;
+//}
+//
+//function processar5(){
+//    document.getElementById("selectDoPeriodo5").disabled = false;
+//    document.getElementById("option1").selected = true;
+//    document.getElementById("option2").selected = true;
+//    document.getElementById("option3").selected = true;
+//    document.getElementById("option4").selected = true;
+//    document.getElementById("selectDoPeriodo1").disabled = true;
+//    document.getElementById("selectDoPeriodo2").disabled = true;
+//    document.getElementById("selectDoPeriodo3").disabled = true;
+//    document.getElementById("selectDoPeriodo4").disabled = true;
+//}
+
+
+
+function processar(number) {
+    let options = [];
+    for(let i = 1; i <= 5; i++) {
+        options.push(document.getElementById('option' + i));
+    }
+    for (let option of options) {
+        if (option.id.substr(-1) === number.toString()) {
+            if (option.selected === false)
+                continue;
+            option.selected = 'true';
+        } else {
+            option.selected = 'false';
+        }
+    }
+    toggleSelects(number);
 }
 
-function processar2(){
-    document.getElementById("selectDoPeriodo2").disabled = false;
-    document.getElementById("option1").selected = true;
-    document.getElementById("option3").selected = true;
-    document.getElementById("option4").selected = true;
-    document.getElementById("option5").selected = true;
-    document.getElementById("selectDoPeriodo1").disabled = true;
-    document.getElementById("selectDoPeriodo3").disabled = true;
-    document.getElementById("selectDoPeriodo4").disabled = true;
-    document.getElementById("selectDoPeriodo5").disabled = true;
+
+function toggleSelects(number) {
+    let selects = [];
+    for(let i = 1; i <= 5; i++) {
+        selects.push(document.getElementById('selectP' + i));
+    }
+    
+    for(let select of selects) {
+        if (select.id.substr(-1) === number.toString()) {
+            select.removeAttribute('disabled');
+        } else {
+            select.setAttribute('disabled', 'true');
+        }
+    }
+    
 }
 
-function processar3(){
-    document.getElementById("selectDoPeriodo3").disabled = false;
-    document.getElementById("option1").selected = true;
-    document.getElementById("option2").selected = true;
-    document.getElementById("option4").selected = true;
-    document.getElementById("option5").selected = true;
-    document.getElementById("selectDoPeriodo1").disabled = true;
-    document.getElementById("selectDoPeriodo2").disabled = true;
-    document.getElementById("selectDoPeriodo4").disabled = true;
-    document.getElementById("selectDoPeriodo5").disabled = true;
-}
-
-function processar4(){
-    document.getElementById("selectDoPeriodo4").disabled = false;
-    document.getElementById("option1").selected = true;
-    document.getElementById("option2").selected = true;
-    document.getElementById("option3").selected = true;
-    document.getElementById("option5").selected = true;
-    document.getElementById("selectDoPeriodo1").disabled = true;
-    document.getElementById("selectDoPeriodo2").disabled = true;
-    document.getElementById("selectDoPeriodo3").disabled = true;
-    document.getElementById("selectDoPeriodo5").disabled = true;
-}
-
-function processar5(){
-    document.getElementById("selectDoPeriodo5").disabled = false;
-    document.getElementById("option1").selected = true;
-    document.getElementById("option2").selected = true;
-    document.getElementById("option3").selected = true;
-    document.getElementById("option4").selected = true;
-    document.getElementById("selectDoPeriodo1").disabled = true;
-    document.getElementById("selectDoPeriodo2").disabled = true;
-    document.getElementById("selectDoPeriodo3").disabled = true;
-    document.getElementById("selectDoPeriodo4").disabled = true;
-}
 
 //function processar1() {
 //    document.getElementById("option2").selected="true";
